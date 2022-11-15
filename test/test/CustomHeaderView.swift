acbb5e0e7c8d6b316ac7c8d357972a9f4012d5b5
rahul Mishra <2111rahulmishra@gmail.com>
2022-11-15 14:03:13 +0530

    Added CollectionVIew cell

---
+++ test/test/CustomHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomHeaderView.swift
+//  test
+//
+//  Created by mac on 15/11/22.
+//
+
+import UIKit
+
+class CustomHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ test/test/LocationTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  LocationTableViewCell.swift
+//  test
+//
+//  Created by mac on 15/11/22.
+//
+
+import UIKit
+
+class LocationTableViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
